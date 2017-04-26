6bc51548c197f52d74f2b4b1a330dce40cd609fc
Stefano Valentini <stefanovalentini94@gmail.com>
2017-04-26 11:47:16 +0200

    Added SessionManager, FreemarkerHelper, FreemarkerCfg

---
+++ src/gamingplatform/controller/SessionManager.java
@@ -0,0 +1,7 @@
+package gamingplatform.controller;
+
+/**
+ * Created by valent0ne on 25/04/2017.
+ */
+public class SessionManager {
+}
---
+++ src/gamingplatform/view/FreemarkerCfg.java
@@ -0,0 +1,7 @@
+package gamingplatform.view;
+
+/**
+ * Created by valent0ne on 24/04/2017.
+ */
+public class FreemarkerCfg {
+}
