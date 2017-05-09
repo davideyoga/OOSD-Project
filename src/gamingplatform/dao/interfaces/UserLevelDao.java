b012d22c9feb599d06d3b57fa76001dcfa1a7e45
davideyoga <d.micarelli7@gmail.com>
2017-05-09 17:03:22 +0200

    fix

--- src/gamingplatform/dao/interfaces/UserLevelDao.java
+++ src/gamingplatform/dao/interfaces/UserLevelDao.java
@@ -1,13 +1,15 @@
 package gamingplatform.dao.interfaces;
 
 import gamingplatform.dao.data.DaoData;
+import gamingplatform.dao.exception.DaoException;
 import gamingplatform.model.Level;
 
-import java.util.Calendar;
+import java.sql.SQLException;
+import java.util.Date;
 import java.util.Map;
 
 public interface UserLevelDao extends DaoData {
 
-    public Map<Calendar,Level> getLevelsByUserId( int userId);
+    public Map<Date,Level> getDateLevelsByUserId(int userId) throws DaoException, SQLException;
 
 }
