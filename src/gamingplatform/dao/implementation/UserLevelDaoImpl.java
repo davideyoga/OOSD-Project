5150ad3f20cc50e7611adcc2ef5ecef68b9e4f06
davideyoga <d.micarelli7@gmail.com>
2017-05-09 16:51:20 +0200

    fix UserLevelDaoImpl

--- src/gamingplatform/dao/implementation/UserLevelDaoImpl.java
+++ src/gamingplatform/dao/implementation/UserLevelDaoImpl.java
@@ -8,9 +8,13 @@ import gamingplatform.model.Level;
 
 import javax.sql.DataSource;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
+
+import static gamingplatform.controller.utils.SecurityLayer.stripSlashes;
 
 public class UserLevelDaoImpl extends DaoDataMySQLImpl implements UserLevelDao {
 
@@ -33,16 +37,55 @@ public class UserLevelDaoImpl extends DaoDataMySQLImpl implements UserLevelDao {
         try {
             super.init(); // connection initialization
 
-            this.selectLevelsByUserId = connection.prepareStatement("");
+            this.selectLevelsByUserId = connection.prepareStatement("SELECT userlevel.date," +
+                                                                                " level.id," +
+                                                                                " level.name," +
+                                                                                " level.trophy," +
+                                                                                " level.icon," +
+                                                                                " level.exp" +
+                                                                        " FROM level LEFT JOIN userlevel ON level.id = userlevel.id_level " +
+                                                                        "WHERE userlevel.id_user = ? ");
 
         } catch (SQLException e) {
-            throw new DaoException("Error initializing user dao", e);
+            throw new DaoException("Error initializing user level dao", e);
         }
     }
-
+    /**
+     * Metodo che restituisce un'insieme di chiave valore che rappresentano la data in cui l'utente con ID userId ha cambiato livello in cui si trova
+     * @return un'insieme di chiave valore che rappresentano la data in cui l'utente con ID userId ha cambiato livello in cui si trova
+     * @throws DaoException lancia eccezione in caso di errore
+     */
     @Override
-    public Map<Calendar, Level> getLevelsByUserId(int userId) {
-        return null;
+    public Map<Date, Level> getDateLevelsByUserId(int userId) throws DaoException{
+
+        Map<Date, Level> map = new HashMap<>();
+
+        try {
+            this.selectLevelsByUserId.setInt(1, userId); //setto la query
+            ResultSet rs = this.selectLevelsByUserId.executeQuery(); //eseguo la query
+
+            while( rs.next() ){ //ciclo ogni tupla del risultato
+
+                Level level = new Level(this); //dichiaro il livello corrispondente alla data da inserire nella mappa
+
+                Date date = rs.getDate("date"); //estraggo la data dal risultato della query
+
+                // setto le variabili del livello da restituire nella mappa
+                level.setId(rs.getInt("id"));
+                level.setName(rs.getInt("name"));
+                level.setTrophy(stripSlashes(rs.getString("trophy")));
+                level.setIcon(stripSlashes(rs.getString("icon")));
+                level.setExp(rs.getInt("exp"));
+
+                map.put(date,level);
+
+            }
+
+        } catch (SQLException e) {
+            throw new DaoException("Error get levels by user id in user level dao", e);
+        }
+
+        return map;
     }
 
     /**
@@ -52,15 +95,13 @@ public class UserLevelDaoImpl extends DaoDataMySQLImpl implements UserLevelDao {
     @Override
     public void destroy() throws DaoException {
 
-        /*
         //chiudo le quary precompilate
         try {
-;
+            this.selectLevelsByUserId.close();
 
         } catch (SQLException e) {
             throw new DaoException("Error destroy dao user", e);
         }
-        */
 
         //chiudo la connessione
         super.destroy();
