bbe26940ae736ec7205fbe3bcf78037883c4c014
Luca Di Gregorio <GregBug@MacBook-Pro-di-Luca.local>
2017-05-15 17:13:49 +0200

    userLevel

--- src/gamingplatform/model/UserLevel.java
+++ src/gamingplatform/model/UserLevel.java
@@ -1,5 +1,6 @@
 package gamingplatform.model;
-
+import gamingplatform.dao.data.DaoData;
+import java.sql.Timestamp;
 /**
  * Created by GregBug on 15/05/2017.
  */
@@ -8,6 +9,26 @@ public class UserLevel {
 
     // Attritubetes
     private int id;
-    private String date;
+    private Timestamp date;
 
+    public UserLevel(DaoData d){
+        this.id = 0;
+        this.date = null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
 }
