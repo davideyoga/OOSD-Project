40f609e558190ddb486409353c58893637d873db
Luca Di Gregorio <GregBug@MacBook-Pro-di-Luca.local>
2017-05-15 16:59:49 +0200

    aggiunto userLevel

---
+++ src/gamingplatform/model/UserLevel.java
@@ -0,0 +1,13 @@
+package gamingplatform.model;
+
+/**
+ * Created by GregBug on 15/05/2017.
+ */
+
+public class UserLevel {
+
+    // Attritubetes
+    private int id;
+    private String date;
+
+}
