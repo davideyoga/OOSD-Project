235383873a62fcf140ae2da7d8e6bc36849f6ed8
Valentina <valentina.cecchini.94@gmail.com>
2017-05-30 13:38:02 +0200

    aggiustato algoritmo per perdere e guadagnare livelli

--- src/gamingplatform/controller/utils/Utils.java
+++ src/gamingplatform/controller/utils/Utils.java
@@ -21,7 +21,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.nio.file.Paths;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -36,36 +38,37 @@ public class Utils {
     /**
      * dato un filePart (file proveniente da una form), controlla se il file è un .jpg
      * se si lo copia nella directory specificata e torna il nome del file salvato
-     * @param filePart oggetto Part contenente dati del file
+     *
+     * @param filePart  oggetto Part contenente dati del file
      * @param directory directory in cui verrà salvto il file
      * @return il nome del file salvato, oppure null su errore
      */
-    public static String fileUpload(Part filePart, String directory, ServletContext svc){
+    public static String fileUpload(Part filePart, String directory, ServletContext svc) {
 
         String fileName = addSlashes(Paths.get(filePart.getSubmittedFileName()).getFileName().toString());
 
         //TODO fare questo controllo lato servlet se no rimette sempre l'immagine di default a ogni update
-        if(isNull(fileName) || fileName.equals("")){
+        if (isNull(fileName) || fileName.equals("")) {
             return "default.png";
         }
 
         //se non è un .jpg oppure un .png
-        if(!(fileName.substring(fileName.length()-4).equals(".jpg") ||
-             fileName.substring(fileName.length()-4).equals(".png"))){
+        if (!(fileName.substring(fileName.length() - 4).equals(".jpg") ||
+                fileName.substring(fileName.length() - 4).equals(".png"))) {
 
-            Logger.getAnonymousLogger().log(java.util.logging.Level.WARNING,"[Utils] fileUpload fallito, formato non corretto: "+fileName+" - formato: "+fileName.substring(fileName.length()-4));
+            Logger.getAnonymousLogger().log(java.util.logging.Level.WARNING, "[Utils] fileUpload fallito, formato non corretto: " + fileName + " - formato: " + fileName.substring(fileName.length() - 4));
             return null;
         }
 
         try {
             //aggiungo il tempo in millisecondi prima del nome (così mantengo il .jpg) per evitare conflitti
-            fileName = System.currentTimeMillis()+fileName;
+            fileName = System.currentTimeMillis() + fileName;
             InputStream fileContent = filePart.getInputStream();
             //salvo il file nella directory specificata
-            File targetFile = new File(svc.getRealPath("template/"+directory+"/"+fileName));
+            File targetFile = new File(svc.getRealPath("template/" + directory + "/" + fileName));
             FileUtils.copyInputStreamToFile(fileContent, targetFile);
         } catch (IOException e) {
-            Logger.getAnonymousLogger().log(java.util.logging.Level.WARNING,"[Utils] fileUpload IOException "+e.getMessage());
+            Logger.getAnonymousLogger().log(java.util.logging.Level.WARNING, "[Utils] fileUpload IOException " + e.getMessage());
             return null;
         }
 
@@ -75,10 +78,10 @@ public class Utils {
 
     public static List<String> getClassFields(Object o) {
         Class<?> clazz = o.getClass();
-        List<String> list= new ArrayList<>();
+        List<String> list = new ArrayList<>();
 
-        for(Field field : clazz.getDeclaredFields()) {
-            if(field.getName().equals("password")){
+        for (Field field : clazz.getDeclaredFields()) {
+            if (field.getName().equals("password")) {
                 continue;
             }
             list.add(field.getName());
@@ -88,10 +91,11 @@ public class Utils {
 
     /**
      * ritorna l'ultimo segmento della url passata
+     *
      * @param url url da analizzare
      * @return ultimo segmento
      */
-    public static String getLastBitFromUrl(final String url){
+    public static String getLastBitFromUrl(final String url) {
         return url.replaceFirst(".*/([^/?]+).*", "$1");
     }
 
@@ -99,19 +103,20 @@ public class Utils {
     /**
      * ritorna l'n-ultimo segmento della url passata
      * es. /terzultimo/penultimo/ultimo con n=1 torna "penultimo"
+     *
      * @param url url da analizzare
-     * @param n indica l'elemento da tornare
+     * @param n   indica l'elemento da tornare
      * @return n-ultimo segmento
      */
-    public static String getNlastBitFromUrl(final String url, int n){
+    public static String getNlastBitFromUrl(final String url, int n) {
 
-        String segment="";
-        int length=0;
+        String segment = "";
+        int length = 0;
 
-        for(int i=0; i<=n; i++){
+        for (int i = 0; i <= n; i++) {
             //System.out.println("passata "+i+", segment: "+segment+", length: "+length);
-            segment=getLastBitFromUrl(url.substring(0,url.length()-length));
-            length+=segment.length()+1; //+1 per il "/"
+            segment = getLastBitFromUrl(url.substring(0, url.length() - length));
+            length += segment.length() + 1; //+1 per il "/"
         }
 
         //System.out.println("return: "+segment);
@@ -119,89 +124,119 @@ public class Utils {
     }
 
 
-    public static int checkLevel( User user ){
+    public static int checkLevel(User user) {
 
         LevelDao ld = new LevelDaoImpl(ds);
         UserDao ud = new UserDaoImpl(ds);
 
+
         List<Level> livelliDaInserire = new ArrayList<>(); //lista dei livelli che l'user ha perso o ha guadagnato
+        int result = 0; //valore che varra' restituito
 
         try {
+            ld.init();
+            ud.init();
 
+            gamingplatform.model.Level livelloAttuale = ud.getLevelByUserId(user.getId());
 
             //dati attuali
             int esperienzaPrecedente = (ud.getLevelByUserId(user.getId())).getExp(); // livello a cui si trovava prima l'utente
             int esperienzaAttuale = user.getExp(); // esperienza a cui si trova attualmente l'utente
-            List<Level> levelList = ld.getLevels(); // lista contenente TUTTI i livelli
+            List<Level> levelList = ld.getLevelsOrdered(); // lista contenente TUTTI i livelli
 
-            int result = 0; //vaore che varra' restituito
+            System.out.println("espPrec: "+ esperienzaPrecedente);
+            System.out.println("espAtt: "+ esperienzaAttuale);
+
 
             //l'utente ha meno esperienza di prima, ha perso livelli
-            if( esperienzaAttuale < esperienzaPrecedente ){
+            if (esperienzaAttuale < esperienzaPrecedente) {
 
-                for( Level l : levelList ){ // ciclo sulla lista dei livelli
+                for (int i = levelList.size() - 1; i >= 0; i--) { // ciclo sulla lista dei livelli
 
-                    if( esperienzaAttuale < l.getExp() && esperienzaPrecedente >= l.getExp()  ){
-                        /* se l'esperieza del livello e' maggiore o uguale a quella precedente dell'utente e
+                    if (levelList.get(i).getExp() >= livelloAttuale.getExp())
+                        continue;
+
+                    if (esperienzaAttuale <= levelList.get(i).getExp()) {
+                        /* se l'esperieza del livello e' maggiore a quella precedente dell'utente e
                            minore o uguale a quella dell'esperienza successiva
                         */
 
-                       result = result - 1;
+                        livelliDaInserire.add(levelList.get(i)); // aggiungo il livello alla lista di livelli persi
+                        System.out.println("livello da inserire("+i+") "+  levelList.get(i).getName()+" "+levelList.get(i).getExp());
 
-                        livelliDaInserire.add(l); // aggiungo il livello alla lista di livelli persi
-
+                    } else {
+                        livelliDaInserire.add(levelList.get(i)); // aggiungo il livello effettivo a cui sono arrivato alla lista di livelli persi
+                        System.out.println("livello da inserire("+i+") "+  levelList.get(i).getName()+" "+levelList.get(i).getExp());
+                        break;
                     }
                 }
-            }else {
+
+
+                result = -1;
+
+            } else if (esperienzaAttuale > esperienzaPrecedente) {
                 //l'utente ha piu' esperienza di prima
-                if (esperienzaAttuale > esperienzaPrecedente) {
 
-                    for (Level l : levelList) { // ciclo sulla lista dei livelli
+                for (int i = 0; i < levelList.size(); i++) {// ciclo sulla lista dei livelli
 
-                        if (esperienzaAttuale >= l.getExp() && esperienzaPrecedente < l.getExp()) {
-                        /* se l'esperieza del livello e' maggiore o uguale a quella precedente dell'utente e
-                           minore o uguale a quella dell'esperienza successiva
-                        */
+                    if (levelList.get(i).getExp() <= livelloAttuale.getExp())
+                        continue;
 
-                            result = result + 1;
+                    if (esperienzaAttuale >= levelList.get(i).getExp()) {
+                            /* se l'esperieza del livello e' maggiore o uguale a quella precedente dell'utente e
+                            minore o uguale a quella dell'esperienza successiva
+                            */
 
-                            livelliDaInserire.add(l); //aggiungo l alla lista dei livelli guadagnati
+                        livelliDaInserire.add(levelList.get(i)); //aggiungo l alla lista dei livelli guadagnati
 
-                        }
                     }
-                }else{
-                    return 0; // se non ha ne guadagnato livelli ne ne ha persi torno 0 e non faccio alcun inserimento in userlevel
                 }
+                result = 1;
+            }else {
+                System.out.println("pippo");
+                ld.destroy();
+                ud.destroy();
+                return 0; // se non ha ne guadagnato livelli ne ne ha persi torno 0 e non faccio alcun inserimento in userlevel
             }
 
-            } catch (DaoException e) {
-            e.printStackTrace();
-        }
 
-        UserLevelDao uld = new UserLevelDaoImpl(ds); // inizializzo un UserlevelDao per accedere al database
+            UserLevelDao uld = new UserLevelDaoImpl(ds); // inizializzo un UserlevelDao per accedere al database
+            uld.init();
 
-        UserLevel ul = uld.getUserLevel(); // user level da inserire
 
-        for(Level l: livelliDaInserire  ){ // scorro i livelli raggiunti dall'utente
+            UserLevel ul = uld.getUserLevel(); // user level da inserire
 
-            ul.setUserId(user.getId()); // inserisco l'utente dentro userLevel
-            ul.setLevelId(l.getId()); // inserisco il livello in userLevel
 
-            try {
+            if(livelliDaInserire.size()==0){
+                return 0;
+            }
+
+
+            for (Level l : livelliDaInserire) { // scorro i livelli raggiunti dall'utente
+
+                System.out.println("livello che inserisco in user level: "+  l.getName()+" "+l.getExp());
+
+                ul.setUserId(user.getId()); // inserisco l'utente dentro userLevel
+                ul.setLevelId(l.getId()); // inserisco il livello in userLevel
+                ul.setDate(new Timestamp(System.currentTimeMillis()));
+
+                java.util.concurrent.TimeUnit.SECONDS.sleep(1);//aspetto almeno 1 secondo
 
                 uld.insertUserlevel(ul); // inserisco lo userLevel precedentemente creato
 
-            } catch (DaoException e) {
-                e.printStackTrace();
-            }//fine try
-        }//fine for di scorrimento di livelliDaInserire
+            }//fine for di scorrimento di livelliDaInserire
 
+            ld.destroy();
+            ud.destroy();
+        } catch (Exception e) {
+            Logger.getAnonymousLogger().log(java.util.logging.Level.WARNING, "[upDateLevel] Eccezione: " + e.getMessage());
+            //torno KO alla chiamata servlet
+            return 500;
+        }
 
-
-        return 0;
+        return result;
     }
 
-
-
-
+//se giochi subito dopo non funziona!!!
+    //controlla
 }
